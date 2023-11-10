e6818e700e1f3f2523127ebe194d77195d5493b2
talsorr <jh345@uakron.edu>
2023-11-10 17:31:57 -0500

    finish hnode.cpp

--- HNode.cpp
+++ HNode.cpp
@@ -5,10 +5,12 @@
 
 // constructor for leaf nodes
 HNode::HNode(const char& s, const int& t) {
-
+	value = s;
+	weight = t;
 }
 
 // constructor for internal nodes
 HNode::HNode(HNode* l, HNode* r) {
-
+	value = '\0';
+	weight = (l->weight + r->weight);
 }
