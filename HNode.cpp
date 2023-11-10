68f9d65ba6a094d30e8a142f64f5729f438ac37c
talsorr <jh345@uakron.edu>
2023-11-10 17:05:36 -0500

    update huffman and hnode cpp

--- HNode.cpp
+++ HNode.cpp
@@ -1,1 +1,14 @@
+// hnode.cpp -- heap node class
+// c. 2015 A. Deeter
 
+#include "HNode.hpp"
+
+// constructor for leaf nodes
+HNode::HNode(const char& s, const int& t) {
+
+}
+
+// constructor for internal nodes
+HNode::HNode(HNode* l, HNode* r) {
+
+}
